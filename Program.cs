40a46593b44c8e2ccfe9090de8d2c3e9ca04c25f
Nevio Pongiluppi <Nevio-source@users.noreply.github.com>
2026-01-20 15:23:58 +0100

    Refactor password generation logic and update documentation

--- Program.cs
+++ Program.cs
@@ -147,18 +147,26 @@ namespace myapp
             Console.Title = "PassGen";
 
 
-            string alphabet =
-                "abcdefghijklmnopqrstuvwxyz" +
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                "0123456789" +
-                "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" +
-                "\u00A7\u00B1\u00A3\u20AC\u00A5\u00A2\u00A9\u00AE\u2122\u00B5\u00B6\u2022\u00B0\u2260\u2248" +
-                "\u00F7\u00D7\u221E\u2264\u2265" +
-                "\u00BF\u00A1\u00DF" +
-                "äöüÄÖÜáàâãåæœØøÅÆŒÐðÞþñÑçÇ" +
-                "\u00AC\u00A6\u2013\u2014\u2026\u2020\u2021\u2030" +
-                "\u00A4";
+        string alphabet =
+            "abcdefghijklmnopqrstuvwxyz" +
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "0123456789" +
+            "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
+        // 2. Unicode-Zeichen (BMP, Surrogates ausgelassen)
+        string unicodeChars =
+            "\u00A0\u00A1\u00A2\u00A3\u00A4\u00A5\u00A6\u00A7\u00A8\u00A9" +
+            "\u00AA\u00AB\u00AC\u00AD\u00AE\u00AF\u00B0\u00B1\u00B2\u00B3" +
+            "\u00B4\u00B5\u00B6\u00B7\u00B8\u00B9\u00BA\u00BB\u00BC\u00BD" +
+            "\u00BE\u00BF\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7" +
+            "\u00C8\u00C9\u00CA\u00CB\u00CC\u00CD\u00CE\u00CF\u00D0\u00D1" +
+            "\u00D2\u00D3\u00D4\u00D5\u00D6\u00D7\u00D8\u00D9\u00DA\u00DB" +
+            "\u00DC\u00DD\u00DE\u00DF\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5" +
+            "\u00E6\u00E7\u00E8\u00E9\u00EA\u00EB\u00EC\u00ED\u00EE\u00EF" +
+            "\u00F0\u00F1\u00F2\u00F3\u00F4\u00F5\u00F6\u00F7\u00F8\u00F9" +
+            "\u00FA\u00FB\u00FC\u00FD\u00FE\u00FF";
+
+        string lexicon = "";
 
 
             while (true)
@@ -166,11 +174,22 @@ namespace myapp
                 Console.Clear();
 
                 Banner();
-                Console.WriteLine("Choose your password length (default 20)");
+                Console.Write("Choose your password length");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("(default 20)");
+                Console.ResetColor();
                 Console.WriteLine("Note: input cannot be higher than 200");
                 Console.WriteLine();
                 Console.Write(">");
                 string lengthInput = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Use Unicode symbols ?");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("(Default N)");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("(Y/N)>");
+                string unicodeyn = Console.ReadLine();
 
                 if (lengthInput == "Nevio_Pongiluppi")
                 {
@@ -188,7 +207,7 @@ namespace myapp
                     }
 
                 }
-
+                //Check input
                 int.TryParse(lengthInput, out int length_re);
                 if (length_re == 0)
                 {
@@ -227,11 +246,23 @@ namespace myapp
                     bytes = Convert.FromHexString(hash);
                 }
 
+                //set lexicon
+                if (unicodeyn == "y" || unicodeyn == "Y")
+                {
+                     lexicon = alphabet + unicodeChars;
+                }
+
+                else
+                {
+                     lexicon = alphabet;
+                }
+
+                //add symbols to passwort ouput
                 List<char> passwordChars = new List<char>();
                 for (int i = 0; i < length_re; i++)
                 {
-                    int index = bytes[i] % alphabet.Length;
-                    passwordChars.Add(alphabet[index]);
+                    int index = bytes[i] % lexicon.Length;
+                    passwordChars.Add(lexicon[index]);
                 }
 
                 string password = new string(passwordChars.ToArray());
